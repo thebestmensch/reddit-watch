1d5bfa9a20c09f5fbad3adc26db9508e80a09ef6
JMensch <james@jamesmensch.com>
2015-04-23 11:09:06 -0400

    added test file, updates to CLI

--- README.md
+++ README.md
@@ -1,7 +1,6 @@
 Reddit-Watch
 =========
 [![Build Status](https://travis-ci.org/JMensch/dust.svg)](https://travis-ci.org/JMensch/reddit-watch)
-[![Coverage Status](https://coveralls.io/repos/JMensch/dust/badge.svg)](https://coveralls.io/r/JMensch/reddit-watch)
 
 Subreddit keyword search and notification bot
 
--- main.py
+++ main.py
@@ -1,6 +1,26 @@
 """
 CLI file for subreddit search and notification bot
 
+@author     james@temboinc.com
+@since      2015-04-23
 """
 
 import optparse
+
+def main():
+	desc = "Reddit-Watch: a subreddit search and notification bot"
+	# the cli
+	p = optparse.OptionParser(description=desc)
+	p.add_option('-s', '--subs', default=False, help="subreddits to search", action="store", dest="subs")
+	p.add_option('-k', '--keywords', default=False, help="keywords to search for", action="store", dest="keys")
+	
+	opts, args = p.parse_args()
+
+	# check required params
+	if not opts.subs:
+		p.error('Please enter a subreddit to watch.')
+	if not opts.keys:
+		p.error('Please enter a keyword for watch for.')
+
+if __name__ == '__main__':
+	main()
